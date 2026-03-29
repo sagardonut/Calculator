637a9b045a9215ded96d2db9de1a5db9d42a6045
Sagar shah4 <fighterworld667@gmail.com>
2026-03-29 14:09:05 +0545

    initial commit

---
+++ calculator.py
@@ -0,0 +1,10 @@
+
+print("Simple Calculator")
+print("Operations: +  -  *  /")
+
+choice = input("Enter operation: ")
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+#write your function on top and run you function below
